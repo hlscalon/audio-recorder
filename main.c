3bda35eb2352c9bcafe782a9c14869fac593e9fc
hlscalon <henscalon@hotmail.com>
2019-11-23 20:50:19 -0300

    Refactoring

--- main.c
+++ main.c
@@ -8,75 +8,119 @@
 
 #define BUFSIZE 1024
 
-/* A simple routine calling UNIX write() in a loop */
-static ssize_t loop_write(int fd, const void*data, size_t size) {
+/**
+ * Write data to files (raw)
+ */
+static ssize_t loop_write_file(FILE * file, const void * data, size_t size) {
     ssize_t ret = 0;
     while (size > 0) {
         ssize_t r;
-        if ((r = write(fd, data, size)) < 0) {
+
+        if ((r = fwrite(data, sizeof data[0], size, file)) < 0) {
             return r;
         }
+
         if (r == 0)
             break;
+
         ret += r;
         data = (const uint8_t*) data + r;
         size -= (size_t) r;
     }
+
     return ret;
 }
 
-int main(int argc, char*argv[]) {
-    /* The sample type to use */
+/**
+ * Open file or create one, if it does not exist
+ */
+FILE * open_file(char * filename) {
+    FILE * f;
+
+    if ((f = fopen(filename, "w+"))) {
+    // if ((f = fopen(filename, "ab+"))) {
+        return f;
+    }
+
+    return NULL;
+}
+
+void print_help() {
+    printf("./audio-recorder <file>\n");
+}
+
+int record_audio(FILE * file) {
+    /**
+     * Signed 16 bits, Little Endian
+     * 44.1kb
+     * Dual channel
+     */
     static const pa_sample_spec ss = {
         .format = PA_SAMPLE_S16LE,
         .rate = 44100,
         .channels = 2
     };
-    pa_simple *s = NULL;
-    int ret = 1;
+
+    pa_simple * s = NULL;
     int error;
+    int ret = 1;
 
-    /* replace STDOUT with the specified file if needed */
-    if (argc > 1) {
-        int fd;
-        if ((fd = open(argv[1], O_WRONLY)) < 0) {
-            fprintf(stderr, __FILE__": open() failed: %s\n", strerror(errno));
-            goto finish;
-        }
-        if (dup2(fd, STDOUT_FILENO) < 0) {
-            fprintf(stderr, __FILE__": dup2() failed: %s\n", strerror(errno));
-            goto finish;
-        }
-        close(fd);
-    }
-
-    /* Create the recording stream */
-    // const char device[] = "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor";
-    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, argv[2], "record", &ss, NULL, NULL, &error))) {
+    // Create the recording stream
+    const char device[] = "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor";
+    if (!(s = pa_simple_new(NULL, "audio-recorder", PA_STREAM_RECORD, device, "record", &ss, NULL, NULL, &error))) {
         fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
-        goto finish;
+        goto finish2;
     }
 
     for (int i = 0;; i++) {
         uint8_t buf[BUFSIZE];
-        /* Record some data ... */
+
+        // Record data
         if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
             fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
-            goto finish;
+            goto finish2;
         }
 
-        /* And write it to STDOUT */
-        if (loop_write(STDOUT_FILENO, buf, sizeof(buf)) != sizeof(buf)) {
+        // Write to file and check if all data was written correctly
+        if (loop_write_file(file, buf, sizeof(buf)) != sizeof(buf)) {
             fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
-            goto finish;
+            goto finish2;
         }
     }
 
     ret = 0;
 
-finish:
-    if (s)
+finish2:
+    if (s) {
         pa_simple_free(s);
+    }
+
+    return ret;
+}
+
+int main(int argc, char * argv[]) {
+    int ret = 1;
+    FILE * file = NULL;
+
+    if (argc < 2) {
+        fprintf(stderr, __FILE__": filename must be informed\n");
+        print_help();
+
+        goto finish;
+    }
+
+    if ((file = open_file(argv[1])) == NULL) {
+        fprintf(stderr, __FILE__": open_file() failed: %s\n", strerror(errno));
+
+        goto finish;
+    }
+
+    ret = record_audio(file);
+
+finish:
+    if (file) {
+        fclose(file);
+    }
 
     return ret;
 }
