644d18116249ece74316e0667b5b7aa181b93de7
hlscalon <henscalon@hotmail.com>
2019-12-24 11:28:02 -0300

    Improvements

--- src/gui_win.c
+++ src/gui_win.c
@@ -1,14 +1,17 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
 #include "gui.h"
 #include "gui_win.h"
+#include "record.h"
 
 struct _AudioRecorderGuiWindow {
     GtkApplicationWindow parent;
 
     GtkWidget *label_result;
 
-    GFile *file;
+    gchar *filename;
+    int count;
 };
 
 G_DEFINE_TYPE(AudioRecorderGuiWindow, audio_recorder_gui_window, GTK_TYPE_APPLICATION_WINDOW);
@@ -19,8 +22,9 @@ GMutex mutex_ui;
 
 typedef struct {
     GtkWidget *label_result;
-    int count;
-    int can_free;
+    gchar *filename;
+    gint count;
+    gint can_free;
 } WorkerData;
 
 static gboolean update_gui(gpointer data) {
@@ -68,13 +72,14 @@ static gpointer record(gpointer data) {
     // wait for free signal
     while (!d->can_free) {}
 
+    g_free(d->filename);
     g_free(d);
 
     return NULL;
 }
 
 static void btn_start_record_clicked(GtkButton *button, AudioRecorderGuiWindow * win) {
-    g_print("Recordddddddddddd\n");
+    g_print("btn-start-record\n");
 
     // ret = record_audio(argv[1]);
 
@@ -82,13 +87,14 @@ static void btn_start_record_clicked(GtkButton *button, AudioRecorderGuiWindow *
     d->label_result = win->label_result;
     d->count = 0;
     d->can_free = 0;
+    d->filename = g_strdup(win->filename);
 
     GThread *thread = g_thread_new("record", record, d);
     g_thread_unref(thread);
 }
 
 static void btn_stop_record_clicked(GtkButton *button, AudioRecorderGuiWindow * win) {
-    g_print("Stopppppppp\n");
+    g_print("btn-stop-record\n");
 
     g_mutex_lock(&mutex_stop);
     stop = 1;
@@ -104,7 +110,6 @@ static void audio_recorder_gui_window_init(AudioRecorderGuiWindow *win) {
 static void audio_recorder_gui_window_class_init(AudioRecorderGuiWindowClass *class) {
     gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/org/gtk/gui/window.ui");
 
-    // gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class), AudioRecorderGuiWindow, btn_record);
     gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class), AudioRecorderGuiWindow, label_result);
 
     gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), btn_start_record_clicked);
@@ -113,7 +118,8 @@ static void audio_recorder_gui_window_class_init(AudioRecorderGuiWindowClass *cl
 
 AudioRecorderGuiWindow * audio_recorder_gui_window_new(AudioRecorderGui *app, gint n_files, GFile ** files) {
     AudioRecorderGuiWindow * win = g_object_new(AUDIO_RECORDER_GUI_WINDOW_TYPE, "application", app, NULL);
-    win->file = files[0];
+    win->filename = g_file_get_basename(files[0]);
+    // g_free(win->filename); // qdo nao usar mais
 
     return win;
 }
